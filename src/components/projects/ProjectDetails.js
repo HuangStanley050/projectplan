47e366ac158f0b9e130c024d067c5c270cc89109
Stanley Huang <infamous_godhand@yahoo.com>
2018-10-07 05:24:10 +0000

    able to direct to specific project page, added conditional rendering, learn new thing Ownprops in mapStateToDispatch

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,23 +1,48 @@
 import React from "react";
+import { connect } from 'react-redux';
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux";
 
 const ProjectDetails = (props) => {
-    const id = props.match.params.id;
-    return (
-        <div className="container sction project-details">
+
+    //insert props.project? to check if the property are valid at the stage of fetching
+    const NotReady = <h1 className="loading">Loading....</h1>;
+
+    if (props.project) {
+        return (
+            <div className="container sction project-details">
             <div className="card z-depth-0">
                 <div className="card-content">
-                 <span class="card-title">Project Title - {id}</span>
+                 <span className="card-title">Project Title - {props.project.title}</span>
                  <p>
                   Bacon ipsum dolor amet beef buffalo corned beef, frankfurter picanha swine doner bacon tri-tip. Pancetta venison t-bone, corned beef pig andouille beef fatback jerky tail chuck meatloaf pastrami buffalo shoulder. Ground round sausage bacon strip steak, swine tail andouille ribeye brisket turducken salami cupim hamburger. Shank beef ground round tail pancetta, bresaola tongue short ribs biltong ham strip steak picanha.
                  </p>
                 </div>
                 <div className="card-action grey lighten-3 black-text">
-                 <div>Posted by Stanley</div>
+                 <div>Posted by {props.project.authorFirstName} {props.project.authorLastName}</div>
                  <div>Posted on September 29th</div>
                 </div>
             </div>
         </div>
-    )
+        )
+    }
+
+    else {
+        return NotReady;
+    }
 };
 
-export default ProjectDetails;
+const mapStateToProps = (state, ownProps) => {
+    //console.log(state);
+    const id = ownProps.match.params.id;
+    const projects = state.firestore.data.projects;
+    const project = projects ? projects[id] : null;
+    return {
+        project: project
+    };
+};
+
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect([{ collection: "projects" }])
+)(ProjectDetails);
--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ProjectSummary from "./ProjectSummary";
 import { convert } from "../../helpers/convertTime";
+import { Link } from "react-router-dom";
 
 //console.log(convert(1538626489));
 
@@ -9,15 +10,17 @@ const ProjectList = ({ data }) => { //destructure data property from props passe
   return (
     <div className="project-list section">
           {data && data.map(project=>
-          { //console.log(project.createdAt.seconds);
+          { console.log(project.id);
             //let time = convert(project.creatAt.seconds);
-            return <ProjectSummary 
-                    key={project.id} 
-                    title={project.title} 
-                    authorFirstname={project.authorFirstName} 
-                    authorLastname={project.authorLastName}
-                    createdAt ={project.createdAt.seconds?convert(project.createdAt.seconds):null}
-                  />;
+            return <Link key={project.title} to={"/project/"+project.id}>
+                      <ProjectSummary 
+                        key={project.id} 
+                        title={project.title} 
+                        authorFirstname={project.authorFirstName} 
+                        authorLastname={project.authorLastName}
+                        createdAt ={project.createdAt.seconds?convert(project.createdAt.seconds):null}
+                      />
+                    </Link>;
               
           }
           )}
