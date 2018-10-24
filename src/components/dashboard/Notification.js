9fd025dad4aebf79c997c01a8cbf7065e2630609
Stanley Huang <infamous_godhand@yahoo.com>
2018-10-24 03:44:19 +0000

    style the Notification component

--- src/components/dashboard/Notification.js
+++ src/components/dashboard/Notification.js
@@ -2,8 +2,18 @@ import React from "react";
 
 const Notifications = (props) => {
     return (
-        <div>
-            <p>Notifications:</p>
+        <div className="section">
+          <div className="card blue lighten-2 z-depth-0">
+            <div className="card-content">
+                <span className="card-title">Notifications</span>
+                <ul className="notifications">
+                    <li>Notification</li>
+                    <li>Notification</li>
+                    <li>Notification</li>
+                    <li>Notification</li>
+                </ul>
+            </div>
+          </div>
         </div>
 
     );
